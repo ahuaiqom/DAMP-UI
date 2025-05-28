7f1e7e90d6e039680917959a08634c9dca0ae624
Archie Saskara <saskaraarchie@gmail.com>
2025-05-28 08:17:44 +0700

    assets and login and splash screen

---
+++ splash.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", () => {
+    const splash = document.getElementById("splash-screen");
+    const main = document.getElementById("main-content");
+    const body = document.getElementsByTagName("body")[0];
+
+    setTimeout(() => {
+        splash.classList.add("fade-out");
+
+        // Wait for the fade-out transition to complete before hiding
+        setTimeout(() => {
+        splash.style.display = "none";
+        main.style.display = "block";
+        }, 1000); // Match transition duration
+    }, 2000); // Delay before starting fade
+
+    setTimeout(() =>{
+        body.style.display = "block";
+    },3000);
+
+});
